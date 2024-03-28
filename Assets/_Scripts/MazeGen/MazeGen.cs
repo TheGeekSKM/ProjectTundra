957e4585a34686b5693fd249de371d9829319039
Chase <chasekat742@gmail.com>
2024-03-28 02:00:17 -0500

    import and adapt maze generator script

---
+++ Assets/_Scripts/MazeGen/MazeGen.cs
@@ -0,0 +1,328 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PieceType { none, oPiece, iPiece, tPiece, xPiece, rPiece };
+
+public class MazeGen : MonoBehaviour
+{
+	//Generation Variables
+	[Header("Generation Variables")]
+	[SerializeField]
+	private bool GenerateRandom;
+	[SerializeField]
+	private GameObject tilePrefab;
+	[SerializeField]
+	private GameObject roomPrefab;
+
+	//Standard width and height of our room prefabs
+	[Space(10)]
+	[SerializeField] [Tooltip("Standard width of our room prefabs")]
+	private int roomWidth;
+	[SerializeField] [Tooltip("Standard height of our room prefabs")]
+	private int roomHeight;
+
+	[Space(10)]
+	[SerializeField] [Tooltip("Total width of the maze")]
+	private int mazeWidth;
+	[SerializeField] [Tooltip("Total height of the maze")]
+	private int mazeHeight;
+
+	//[Space(10)]
+	//[SerializeField]
+	//private bool doesStart;
+	//[SerializeField]
+	//private Vector2 startCoord;
+	//[SerializeField]
+	//private bool doesEnd;
+	//[SerializeField]
+	//private Vector2 endCoord;
+
+	private void Start()
+	{
+		if (GenerateRandom)
+		{
+			//ensure maze has generation dimensions
+			if (mazeWidth == 0 || mazeHeight == 0)
+			{
+				Debug.LogError("Set the dimensions, dummy");
+				Debug.Break();
+			}
+
+			//start generation
+			StartCoroutine(GenerateRooms());
+		}
+		else
+		{
+			//TODO
+		}
+	}
+
+	//Maze generation adapted from u/DavoMyan on Reddit (and also myself from last semester)
+	class Tile
+	{
+		public int x;
+		public int y;
+		public int[] exits = new int[4];
+		public bool visited;
+		public bool marked;
+	}
+	private List<Tile> visitedCells;
+	private List<Tile> markedCells;
+	private Tile currentCell;
+	private bool finishedGeneration = false;
+	private Tile[,] grid;
+	void GenerateMaze()
+	{
+		//create a "Tile" array of maze width & height
+		grid = new Tile[mazeWidth, mazeHeight];
+
+		//populate the "Tile" array by instantiating tile objects & assigning them to each slot
+		for (int ny = 0; ny < mazeWidth; ny++) {
+			for (int nx = 0; nx < mazeHeight; nx++)
+			{
+				GameObject go = (GameObject)Instantiate(tilePrefab, transform.TransformPoint(new Vector3(nx*roomWidth, ny*roomHeight, 0)), Quaternion.identity, transform);
+				Tile tile = go.GetComponent<Tile>();
+				tile.x = nx; tile.y = ny;
+
+				grid[nx, ny] = tile;
+			}
+		}
+
+		//Bools for tile visitation during generation
+		bool northVisited;
+		bool eastVisited;
+		bool southVisited;
+		bool westVisited;
+
+		//Lists to check off which tiles are visited, & which are marked to be visited soon
+		visitedCells = new List<Tile>();
+		markedCells = new List<Tile>();
+
+		//Pick a tile, any tile!
+		System.Random r = new System.Random();
+		int x = Random.Range(0, mazeWidth);
+		int y = Random.Range(0, mazeHeight);
+
+		//Register chosen tile as current, and add it to visited
+		currentCell = grid[x, y];
+		currentCell.visited = true;
+		visitedCells.Add(currentCell);
+
+		//Here we go! (I think I made this a coroutine because it was breaking last I used it?)
+		StartCoroutine(GenerateLogic());
+		IEnumerator GenerateLogic()
+		{
+			do
+			{
+				x = currentCell.x;
+				y = currentCell.y;
+
+				//If tile is not on the left edge
+				if (x != 0)
+				{
+					//If the tile to the left has not been marked or visited
+					if (!grid[x - 1, y].marked && !grid[x - 1, y].visited)
+					{
+						//mark the tile to the left
+						grid[x - 1, y].marked = true;
+						markedCells.Add(grid[x - 1, y]);
+					}
+				}
+				//If tile is not on right edge
+				if (x != mazeWidth - 1)
+				{
+					//If the tile to the right has not been marked or visited
+					if (!grid[x + 1, y].marked && !grid[x + 1, y].visited)
+					{
+						//mark the tile to the right
+						grid[x + 1, y].marked = true;
+						markedCells.Add(grid[x + 1, y]);
+					}
+				}
+				//If tile is not on bottom edge
+				if (y != 0)
+				{
+					//If the tile below has not been marked or visited
+					if (!grid[x, y - 1].marked && !grid[x, y - 1].visited)
+					{
+						//mark the tile below
+						grid[x, y - 1].marked = true;
+						markedCells.Add(grid[x, y - 1]);
+					}
+				}
+				//If tile is not on top edge
+				if (y != mazeHeight - 1)
+				{
+					//If the tile above has not been marked or visited
+					if (!grid[x, y + 1].marked && !grid[x, y + 1].visited)
+					{
+						//mark the tile above
+						grid[x, y + 1].marked = true;
+						markedCells.Add(grid[x, y + 1]);
+					}
+				}
+
+				//Pick a random marked tile to become new current tile
+				int rMarked = r.Next(markedCells.Count);
+				currentCell = markedCells[rMarked];
+				ResetVisited();
+
+				////Debug stuff for generation visualization
+				//Debug.Log(string.Format("Marked cells around ({0},{1})",x,y));
+				//yield return new WaitForFixedUpdate();
+
+				//Time to generate exits
+				do
+				{
+					//yield return new WaitForFixedUpdate();
+
+					//Pick a random direction
+					int direction = Random.Range(0, 4);
+					//Debug.Log("Visiting Adjacent, direction " + direction);
+
+					//Based on picked direction:
+					switch (direction)
+					{
+						//North
+						case 0:
+							//If the current tile is on top edge or the tile above hasn't been visited, try again
+							if (currentCell.y == mazeHeight - 1 || !grid[currentCell.x, currentCell.y + 1].visited)
+								break;
+							else
+							{
+								//Open up north exit for current tile, and south exit for the tile above
+								grid[currentCell.x, currentCell.y].exits[0] = 1;
+								grid[currentCell.x, currentCell.y + 1].exits[2] = 1;
+
+								//Add current tile to visted tiles, remove marking
+								visitedCells.Add(grid[currentCell.x, currentCell.y]);
+								grid[currentCell.x, currentCell.y].visited = true;
+								markedCells.Remove(currentCell);
+
+								//The North is clear!!
+								northVisited = true;
+							}
+							break;
+						//East
+						case 1:
+							//If the current tile is on right edge or the tile to the right hasn't been visited, try again
+							if (currentCell.x == mazeWidth - 1 || !grid[currentCell.x + 1, currentCell.y].visited)
+								break;
+							else
+							{
+								//Open up east exit for current tile, and west exit for the tile to the right
+								grid[currentCell.x, currentCell.y].exits[1] = 1;
+								grid[currentCell.x + 1, currentCell.y].exits[3] = 1;
+
+								//Add current tile to visited tiles, remove marking
+								visitedCells.Add(grid[currentCell.x, currentCell.y]);
+								grid[currentCell.x, currentCell.y].visited = true;
+								markedCells.Remove(currentCell);
+
+								//The East is clear!!
+								eastVisited = true;
+							}
+							break;
+						//South
+						case 2:
+							//If the current tile is on bottom edge or the tile below hasn't been visited, try again
+							if (currentCell.y == 0 || !grid[currentCell.x, currentCell.y - 1].visited)
+								break;
+							else
+							{
+								//Open up south exit for current tile, and north exit for the tile below
+								grid[currentCell.x, currentCell.y].exits[2] = 1;
+								grid[currentCell.x, currentCell.y - 1].exits[0] = 1;
+
+								//Add current tile to visited tiles, remove marking
+								visitedCells.Add(grid[currentCell.x, currentCell.y]);
+								grid[currentCell.x, currentCell.y].visited = true;
+								markedCells.Remove(currentCell);
+
+								//The South is clear!!
+								southVisited = true;
+							}
+							break;
+						//West
+						case 3:
+							//If the current tile is on left edge or the tile to the left hasn't been visited, try again
+							if (currentCell.x == 0 || !grid[currentCell.x - 1, currentCell.y].visited)
+								break;
+							else
+							{
+								//Open up west exit for current tile, and east exit for the tile to the left
+								grid[currentCell.x, currentCell.y].exits[3] = 1;
+								grid[currentCell.x - 1, currentCell.y].exits[1] = 1;
+
+								//Add current tile to visited tiles, remove marking
+								visitedCells.Add(grid[currentCell.x, currentCell.y]);
+								grid[currentCell.x, currentCell.y].visited = true;
+								markedCells.Remove(currentCell);
+
+								//The West is clear!!
+								westVisited = true;
+							}
+							break;
+						default:
+							break;
+					}
+
+					//Loop this until all directions for this tile are cleared!
+				} while (!northVisited && !eastVisited && !southVisited && !westVisited);
+
+				//Loop this until all tiles have been visited!
+			} while (visitedCells.Count < mazeWidth * mazeHeight);
+
+			////Special case for start & end
+			//if (doesStartToEnd)
+			//{
+			//	grid[(int)startCoord.x, (int)startCoord.y].exits[3] = 1;
+			//	grid[(int)endCoord.x, (int)endCoord.y].exits[1] = 1;
+			//}
+
+			//We're done generating the maze!
+			finishedGeneration = true;
+			yield return null;
+		}
+
+		//Making sure a new tile is ready for visitation
+		void ResetVisited()
+		{
+			northVisited = false;
+			eastVisited = false;
+			southVisited = false;
+			westVisited = false;
+		}
+	}
+
+	//Generate Rooms
+	IEnumerator GenerateRooms()
+	{
+		//Generate maze exits, do not proceed until this is done
+		GenerateMaze();
+		yield return new WaitUntil(() => finishedGeneration);
+
+		//puzzle.pieces = new PieceController[puzzle.width, puzzle.height];
+
+		//foreach (Tile tile in grid)
+		//{
+		//	int exitSum = tile.exits[0] + tile.exits[1] + tile.exits[2] + tile.exits[3];
+		//	if (exitSum == 2 && tile.exits[0] != tile.exits[2])
+		//		exitSum = 5;
+
+		//	//Set piece type and setup piece
+		//	GameObject go = (GameObject)Instantiate(piecePrefab, transform.TransformPoint(new Vector3(tile.x, tile.y, 0)), Quaternion.identity, transform);
+		//	go.name = string.Format("({0},{1})",tile.x,tile.y);
+		//	PieceController piece = go.GetComponent<PieceController>();
+		//	piece.pieceType = (PieceType)exitSum;
+		//	piece.x = tile.x;
+		//	piece.y = tile.y;
+		//	piece.Setup();
+
+		//	puzzle.pieces[tile.x, tile.y] = piece;
+
+		//	Destroy(tile.gameObject);
+		//}
+	}
+}
