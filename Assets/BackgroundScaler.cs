624d8d3faa278d2abb919d0a92bbc99b5612e151
Chase S <chasekat742@gmail.com>
2024-04-11 10:48:23 -0500

    changes to background scaler
    made it better

--- Assets/BackgroundScaler.cs
+++ Assets/BackgroundScaler.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using UnityEngine.U2D;
 using UnityEngine.UI;
 
-[ExecuteAlways]
 public class BackgroundScaler : MonoBehaviour
 {
 	[SerializeField] private CanvasScaler scaler;
 	[SerializeField] private PixelPerfectCamera ppCamera;
 
-	private void Update()
+	private void OnValidate()
+	{
+		if (ppCamera != null)
+		{
+			if (scaler.scaleFactor != ppCamera.pixelRatio)
+				scaler.scaleFactor = ppCamera.pixelRatio;
+		}
+	}
+
+	private void Awake()
 	{
 		if (ppCamera != null)
 		{
