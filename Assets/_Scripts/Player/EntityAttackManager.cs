f24883a6cd6abd108fe5e96592c7c0dda288e0d0
Sai Mangipudi <thegeekskm101@gmail.com>
2024-04-10 21:17:38 -0500

    Fixed Bug Where Enemy Constantly Attacks

--- Assets/_Scripts/Player/EntityAttackManager.cs
+++ Assets/_Scripts/Player/EntityAttackManager.cs
@@ -47,7 +47,10 @@ public class EntityAttackManager : MonoBehaviour
     public void Attack()
     {
         // if not in combat state, player cannot attack
-        if (_ignoreInput) return; 
+        if (_ignoreInput) return;
+
+        // if player has no action points, player cannot attack
+        if (_entityStatsContainer.PlayerStatsData.CurrentActionPoints <= 0) return;
 
         // get the weapon from the player's inventory
         var weapon = _entityStatsContainer.ItemContainer.GetWeapon();
