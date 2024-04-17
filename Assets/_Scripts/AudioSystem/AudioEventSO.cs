f7ea120aab1b32a15e908c49db7be1feb1cbae68
Sai Mangipudi <thegeekskm101@gmail.com>
2024-04-17 15:21:57 -0500

    AudioManager
    Needs documentation

---
+++ Assets/_Scripts/AudioSystem/AudioEventSO.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioEvent", menuName = "Audio/AudioEvent")]
+public class AudioEventSO : ScriptableObject
+{
+    public EAudioEvent audioEvent;
+    public AudioClip audioClip;
+}
+
+public enum EAudioEvent
+{
+    ButtonClick,
+    PlayerHurt,
+    PlayerAttack
+}
