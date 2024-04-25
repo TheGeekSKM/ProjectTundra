95210fc51cdea521360dd4052262aad58873bb1d
Sai <thegeekskm101@gmail.com>
2024-04-24 20:50:18 -0500

    Fixed Scene Transition and Item Loop Iteration Bug

--- Assets/_Scripts/Items/ItemContainer.cs
+++ Assets/_Scripts/Items/ItemContainer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
@@ -22,14 +23,19 @@ public class ItemContainer
     /// <param name="item">The item to add.</param>
     public void AddItem(BaseItemData item)
     {
-        foreach (var i in Items)
+        // If the item is a weapon, replace the existing weapon.
+        foreach (var i in Items.ToList())
         {
             if (i is WeaponItemData)
             {
                 int indexOfWeapon = Items.IndexOf(i);
                 SetItem(indexOfWeapon, item);
+                OnItemContainerChanged?.Invoke(item);
+                return;
             }
         }
+
+        // If the item is not a weapon, add it to the container.
         Items.Add(item);
         OnItemContainerChanged?.Invoke(item);
     }
